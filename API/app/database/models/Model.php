81b38c73ae53a21ec40ed51df4e2d7d45a01adfd
Gui-leite233 <89623564+Gui-leite233@users.noreply.github.com>
2023-08-16 16:54:28 -0300

    Add files via upload

---
+++ API/app/database/models/Model.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\database\models;
+
+use app\traits\Connection;
+
+use app\traits\Read;
+
+
+abstract class Model {
+    use Read, Connection;
+}
---
+++ API/app/database/models/Student.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace app\database\models;
+
+class Student extends Model {
+    protected $table = 'students';
+}
